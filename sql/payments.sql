efc42280a3bfb0818b3d46a0b08d7516529df145
andrebrener <brener.andre@gmail.com>
2018-11-05 15:11:17 -0300

    Add amount to payments

--- sql/payments.sql
+++ sql/payments.sql
@@ -1,6 +1,7 @@
 CREATE TABLE payments (
  id serial PRIMARY KEY,
  client_id int NOT NULL REFERENCES clients(id),
+ amount decimal NOT NULL,
  date TIMESTAMP NOT NULL,
  created_on TIMESTAMP NOT NULL,
  method text NOT NULL
