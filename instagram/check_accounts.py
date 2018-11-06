b1617a0853cc6b1222dbc15cc00c6d1530656f71
andrebrener <brener.andre@gmail.com>
2018-11-05 22:30:43 -0300

    :echohl WarningMsg|echo ':Gstatus C is deprecated in favor of cc'|echohl NONE Improve check accounts

--- instagram/check_accounts.py
+++ instagram/check_accounts.py
@@ -5,20 +5,20 @@ import logging
 import logging.config
 
 from datetime import datetime
+from dino_data import DINO_USER, DINO_PASS
 
 import pandas as pd
 
 from instapy import InstaPy
 
 from main import get_media_id
-from functions import get_user_id, LIB_COMMON_DIR, PROJECT_DIR
-from user_data import user_data
+from functions import LIB_COMMON_DIR, PROJECT_DIR
 
 for p in [LIB_COMMON_DIR, PROJECT_DIR]:
     sys.path.append(p)
 
 from config import config
-from db_handle import insert_values
+from db_handle import get_df_from_query, insert_values
 
 logger = logging.getLogger('main_logger')
 
@@ -27,18 +27,29 @@ os.makedirs(LOGS_DIR, exist_ok=True)
 os.chdir(LOGS_DIR)
 
 
+def get_all_clients():
+    clients = get_df_from_query(
+        "select id, ig_username from clients where ig_username is not null"
+    )
+
+    return clients
+
+
 def get_users_accounts():
     media_id = get_media_id()
 
-    for username, vals in user_data.items():
+    session = InstaPy(
+        username=DINO_USER, password=DINO_PASS, headless_browser=True
+    )
+
+    clients = get_all_clients()
+    for row in clients.iterrows():
+        vals = row[1]
+        username = vals['ig_username']
+        client_id = vals['id']
+
         logger.info("Getting data for {}".format(username))
-        key = vals['key']
 
-        session = InstaPy(
-            username=username, password=key, headless_browser=True
-        )
-
-        client_id = get_user_id(username)
         followers_count, follows_count = session.get_follow_count(username)
         # followers = session.grab_followers(
         # username=username,
@@ -51,11 +62,12 @@ def get_users_accounts():
             'client_id': [client_id],
             'media_id': [media_id],
             'followers': [followers_count],
-            'follows': [len(follows_count)],
+            'follows': [follows_count],
             # 'follower_users': [json.dumps(followers)],
             'follower_users': [],
             'created_on': [datetime.now()]
         }
+        import pdb; pdb.set_trace()  # noqa # yapf: disable
 
         client_df = pd.DataFrame(client_dict)
         insert_values(client_df, 'clients_accounts')
